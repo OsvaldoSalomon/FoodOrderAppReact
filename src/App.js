1c4fe4086a0846c8bdd7fc02fe2ce961a660ce28
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2021-08-05 13:14:44 -0400

    Added a lot of custom components, the page is already looking very good.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import Header from "./components/Layout/Header";
+import {Fragment} from "react";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+    return (
+        <Fragment>
+            <Cart/>
+            <Header/>
+            <main>
+                <Meals/>
+            </main>
+        </Fragment>
+    );
 }
 
 export default App;
