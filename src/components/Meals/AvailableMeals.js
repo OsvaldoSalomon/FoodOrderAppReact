834fac91b6c2200f19a74138c1b717048cc69c8b
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2021-08-16 05:59:26 -0400

    The Meals are fetched by Firebase now.

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,40 +1,32 @@
+import {useEffect, useState} from 'react';
 import classes from './AvailableMeals.module.css';
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Traditional Japanese dish of prepared vinegared rice, usually with some sugar and salt, ' +
-            'accompanying a variety of ingredients, such as seafood, often raw, and vegetables.',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A schnitzel is a thin slice of meat. The meat is usually thinned by pounding with a meat tenderizer. ' +
-            'Most commonly, the meats are breaded before frying. The breaded schnitzel is popular in many countries.',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'Typically considered a sandwich, consisting of one or more cooked patties of ground meat, ' +
-            'usually beef, placed inside a sliced bread roll or bun.',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Curry',
-        description: 'Curry is a variety of dishes originating in the Indian subcontinent. It uses a combination of spices or herbs, ' +
-            'usually including ground turmeric, cumin, coriander, ginger, and fresh or dried chilies.',
-        price: 18.99,
-    },
-];
-
 function AvailableMeals() {
-    const mealsList = DUMMY_MEALS.map(meal =>
+    const [meals, setMeals] = useState([])
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch('https://react-http-f7e81-default-rtdb.firebaseio.com/meals.json').then();
+            const responseData = await response.json();
+
+            const loadedMeals = [];
+
+            for (const key in responseData) {
+                loadedMeals.push({
+                    id: key,
+                    name: responseData[key].name,
+                    description: responseData[key].description,
+                    price: responseData[key].price,
+                })
+            }
+            setMeals(loadedMeals);
+        }
+        fetchMeals();
+    }, []);
+
+    const mealsList = meals.map(meal =>
         <MealItem
             id={meal.id}
             key={meal.id}
